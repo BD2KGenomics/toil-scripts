d1d2d3c08ac29fad1cd699a06328ca89bc13bca9
Jacob Pfeil <pfeil.jj@gmail.com>
2015-10-20 13:30:01 -0700

    Updated URLs in germline launch script

--- gatk-germline/launch_germ.sh
+++ gatk-germline/launch_germ.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-export TMPDIR=/home/jacob/fake_mnt
+export TMPDIR=./fake_mnt
 python germ.py \
 $TMPDIR/jstore \
 --config "config.txt" \
---reference "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz" \
---phase "ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.8/b37/1000G_phase1.indels.b37.vcf.gz" \
---mills "ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.8/b37/Mills_and_1000G_gold_standard.indels.b37.vcf.gz" \
---dbsnp "ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.8/b37/dbsnp_138.b37.vcf.gz" \
---hapmap "ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.8/b37/hapmap_3.3.b37.vcf.gz" \
---omni "ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.8/b37/1000G_omni2.5.b37.vcf.gz" \
---output_dir '/home/jacob/' \
+--reference "https://s3-us-west-2.amazonaws.com/cgl-pipeline-inputs/germline/hs37d5.fa" \
+--phase "https://s3-us-west-2.amazonaws.com/cgl-pipeline-inputs/germline/1000G_phase1.indels.b37.vcf" \
+--mills "https://s3-us-west-2.amazonaws.com/cgl-pipeline-inputs/germline/Mills_and_1000G_gold_standard.indels.b37.vcf" \
+--dbsnp "https://s3-us-west-2.amazonaws.com/cgl-pipeline-inputs/germline/dbsnp_138.b37.vcf" \
+--hapmap "https://s3-us-west-2.amazonaws.com/cgl-pipeline-inputs/germline/hapmap_3.3.b37.vcf" \
+--omni "https://s3-us-west-2.amazonaws.com/cgl-pipeline-inputs/germline/1000G_omni2.5.b37.vcf" \
+-o '/home/jacob/' \
