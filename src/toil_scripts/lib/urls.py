202bfa47581e81c46e13810cf0b82655249989c2
John Vivian <jtvivian@gmail.com>
2016-04-13 13:39:37 -0700

    Change RuntimeError to ValueError in _download_s3_url()

--- src/toil_scripts/lib/urls.py
+++ src/toil_scripts/lib/urls.py
@@ -82,8 +82,8 @@ def _download_s3_url(file_path, url, ceph=None):
     try:
         parsed_url = urlparse(url)
         if not parsed_url.netloc or not parsed_url.path.startswith('/'):
-            raise RuntimeError("An S3 URL must be of the form s3:/BUCKET/ or "
-                               "s3://BUCKET/KEY. '%s' is not." % url)
+            raise ValueError("An S3 URL must be of the form s3:/BUCKET/ or "
+                             "s3://BUCKET/KEY. '%s' is not." % url)
         bucket = s3.get_bucket(parsed_url.netloc)
         key = bucket.get_key(parsed_url.path[1:])
         key.get_contents_to_filename(file_path)
