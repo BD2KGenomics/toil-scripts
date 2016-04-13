bb75648157e216eaab02ef98ccee86829bff7dcf
John Vivian <jtvivian@gmail.com>
2016-04-13 13:27:05 -0700

    Clarify docstring

--- src/toil_scripts/lib/files.py
+++ src/toil_scripts/lib/files.py
@@ -54,7 +54,7 @@ def consolidate_tarballs_job(job, **fname_to_id):
     Subdirs within the tarball will be named the keys in **fname_to_id
 
     :param dict[str,str] fname_to_id: Key word arguments of the form: file_name_prefix=FileStoreID
-    :return str: FileStoreID
+    :return str: The file store ID of the generated tarball
     """
     work_dir = job.fileStore.getLocalTempDir()
     # Retrieve output file paths to consolidate
