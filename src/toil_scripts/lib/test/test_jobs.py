41b06d8a7cf2916c1cbfa2f02fdede475761b620
John Vivian <jtvivian@gmail.com>
2016-06-29 17:35:28 -0700

    Add disk to test_map_job

--- src/toil_scripts/lib/test/test_jobs.py
+++ src/toil_scripts/lib/test/test_jobs.py
@@ -2,11 +2,11 @@ import os
 from toil.job import Job
 
 
-def test_sample_batcher(tmpdir):
+def test_map_job(tmpdir):
     from toil_scripts.lib.jobs import map_job
     options = Job.Runner.getDefaultOptions(os.path.join(str(tmpdir), 'test_store'))
     samples = [x for x in xrange(200)]
-    j = Job.wrapJobFn(map_job, _test_batch, samples, 'a', 'b', 'c')
+    j = Job.wrapJobFn(map_job, _test_batch, samples, 'a', 'b', 'c', disk='1K')
     Job.Runner.startToil(j, options)
 
 
