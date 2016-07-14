de65b30b472a571a5803dfd7a7bb69d7b43d6b30
Hannes Schmidt <hannes@ucsc.edu>
2016-07-13 20:09:19 -0700

    Convert RNASeq test to TestCase and randomize output (resolves #354)
    Using a class-based unit test just makes it easier to control what is initialized at which stage of the the test lifecycle. It also should open us up to provide a shared TestCase base class in toil-lib.

--- src/toil_scripts/rnaseq_cgl/test/test_rnaseq_cgl.py
+++ src/toil_scripts/rnaseq_cgl/test/test_rnaseq_cgl.py
@@ -3,7 +3,9 @@ import shutil
 import subprocess
 import textwrap
 from contextlib import closing
+from unittest import TestCase
 from urlparse import urlparse
+from uuid import uuid4
 
 import os
 import posixpath
@@ -12,77 +14,74 @@ from toil_scripts.lib import get_work_directory
 
 log = logging.getLogger(__name__)
 
-input_dir = urlparse('s3://cgl-pipeline-inputs/rnaseq_cgl/ci')
-output_dir = urlparse('s3://cgl-driver-projects/test/ci')
-sample = urlparse(input_dir.geturl() + '/chr6_sample.tar.gz')
 
+class RNASeqCGLTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(RNASeqCGLTest, cls).setUpClass()
+        # FIXME: pull up into common base class
+        logging.basicConfig(level=logging.INFO)
 
-def test_rnaseq_cgl():
-    logging.basicConfig(level=logging.INFO)  # FIXME: I wish we didn't have to do this here
-    workdir = get_work_directory()
-    try:
-        # Call Pipeline
-        base_command = ['toil-rnaseq', 'run',
-                        '--config', generate_config(workdir),
-                        os.path.join(workdir, 'jstore'),
-                        '--retryCount', '1',
-                        '--workDir', workdir]
-        # Run with --samples
-        try:
-            subprocess.check_call(base_command + ['--samples', sample.geturl()])
-        finally:
-            cleanup_and_validate()
-        # Run with manifest
-        try:
-            subprocess.check_call(base_command + ['--manifest', generate_manifest(workdir)])
-        finally:
-            cleanup_and_validate()
-    finally:
-        shutil.rmtree(workdir)
+    def setUp(self):
+        self.input_dir = urlparse('s3://cgl-pipeline-inputs/rnaseq_cgl/ci')
+        self.output_dir = urlparse('s3://cgl-driver-projects/test/ci/%s' % uuid4())
+        self.sample = urlparse(self.input_dir.geturl() + '/chr6_sample.tar.gz')
+        self.workdir = get_work_directory()
+        self.base_command = ['toil-rnaseq', 'run',
+                             '--config', self._generate_config(),
+                             os.path.join(self.workdir, 'jobstore'),
+                             '--retryCount', '1',
+                             '--workDir', self.workdir]
 
+    def test_with_samples_option(self):
+        subprocess.check_call(self.base_command + ['--samples', self.sample.geturl()])
+        self._assertOutput()
 
-def cleanup_and_validate():
-    valid_output = False
-    expected_output = posixpath.basename(sample.path)
-    with closing(S3Connection()) as s3:
-        b = Bucket(s3, output_dir.netloc)
-        path = output_dir.path[1:]
-        for k in b.list(prefix=path):
-            assert k.name.startswith(path)
-            if k.name[len(path):] == '/' + expected_output:
-                # FIXME: We may want to validate the output a bit more
-                valid_output = True
-            else:
-                log.warn('Unexpected output file %s/%s', output_dir.geturl(), k.name)
-            k.delete()
-    assert valid_output, 'Did not find expected output file'
+    def test_with_manifest(self):
+        subprocess.check_call(self.base_command + ['--manifest', self._generate_manifest()])
+        self._assertOutput()
 
+    def _assertOutput(self):
+        with closing(S3Connection()) as s3:
+            bucket = Bucket(s3, self.output_dir.netloc)
+            prefix = self.output_dir.path[1:]
+            output_file = posixpath.basename(self.sample.path)
+            bucket.get_key(posixpath.join(prefix, output_file), validate=True)
+            # FIXME: We may want to validate the output a bit more
 
-def generate_config(workdir):
-    path = os.path.join(workdir, 'config-toil-rnaseq.yaml')
-    with open(path, 'w') as f:
-        f.write(textwrap.dedent("""
-            star-index: {input_dir}/starIndex_chr6.tar.gz
-            kallisto-index: s3://cgl-pipeline-inputs/rnaseq_cgl/kallisto_hg38.idx
-            rsem-ref: {input_dir}/rsem_ref_chr6.tar.gz
-            output-dir:
-            s3-output-dir: {output_dir}
-            fastqc: true
-            cutadapt:
-            ssec:
-            gtkey:
-            wiggle:
-            save-bam:
-            fwd-3pr-adapter: AGATCGGAAGAG
-            rev-3pr-adapter: AGATCGGAAGAG
-            ci-test: true
-            """[1:]).format(output_dir=output_dir.geturl(),
-                            input_dir=input_dir.geturl()))
-    return path
+    def tearDown(self):
+        shutil.rmtree(self.workdir)
+        with closing(S3Connection()) as s3:
+            bucket = Bucket(s3, self.output_dir.netloc)
+            prefix = self.output_dir.path[1:]
+            for key in bucket.list(prefix=prefix):
+                assert key.name.startswith(prefix)
+                key.delete()
 
+    def _generate_config(self):
+        path = os.path.join(self.workdir, 'config-toil-rnaseq.yaml')
+        with open(path, 'w') as f:
+            f.write(textwrap.dedent("""
+                    star-index: {input_dir}/starIndex_chr6.tar.gz
+                    kallisto-index: s3://cgl-pipeline-inputs/rnaseq_cgl/kallisto_hg38.idx
+                    rsem-ref: {input_dir}/rsem_ref_chr6.tar.gz
+                    output-dir:
+                    s3-output-dir: {output_dir}
+                    fastqc: true
+                    cutadapt:
+                    ssec:
+                    gtkey:
+                    wiggle:
+                    save-bam:
+                    fwd-3pr-adapter: AGATCGGAAGAG
+                    rev-3pr-adapter: AGATCGGAAGAG
+                    ci-test: true
+                    """[1:]).format(output_dir=self.output_dir.geturl(),
+                                    input_dir=self.input_dir.geturl()))
+        return path
 
-def generate_manifest(workdir):
-    path = os.path.join(workdir, 'manifest-toil-rnaseq.tsv')
-    with open(path, 'w') as f:
-        f.write('\t'.join(['tar', 'paired', 'chr6_sample', sample.geturl()]))
-    return path
+    def _generate_manifest(self):
+        path = os.path.join(self.workdir, 'manifest-toil-rnaseq.tsv')
+        with open(path, 'w') as f:
+            f.write('\t'.join(['tar', 'paired', 'chr6_sample', self.sample.geturl()]))
+        return path
