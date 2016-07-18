c157e0631ecfc3a66aa32e23d14421444192262d
Hannes Schmidt <hannes@ucsc.edu>
2016-07-18 16:36:12 -0700

    Prevent jenkins.sh from reusing venvs from a previous, failed build

--- jenkins.sh
+++ jenkins.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 # Create s3am venv
+rm -rf s3am
 virtualenv s3am
 s3am/bin/pip install s3am==2.0a1.dev93
-# Expose binaries to the PATH
+# Expose s3am binary on the PATH
+rm -rf bin
 mkdir bin
 ln -snf ${PWD}/s3am/bin/s3am bin/
 export PATH=$PATH:${PWD}/bin
 
 # Create Toil venv
+rm -rf venv
 virtualenv venv
 . venv/bin/activate
 # Adding AWS extra to get boto as required by tests
@@ -24,8 +27,8 @@ make develop
 make test
 make clean
 
+# clean working copy to satisfy corresponding check in Makefile
 rm -rf bin s3am
 make pypi
-rm -rf venv
 
-rm -rf $TMPDIR
+rm -rf venv $TMPDIR
