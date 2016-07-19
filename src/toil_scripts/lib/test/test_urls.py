699bc734877c7bf64222fc9c5929fa0550863358
John Vivian <jtvivian@gmail.com>
2016-07-19 13:31:18 -0700

    Change test to use s3am for encrypted downloads
    Add pre-check for key as upload fails if encryption keys differ

--- src/toil_scripts/lib/test/test_urls.py
+++ src/toil_scripts/lib/test/test_urls.py
@@ -22,6 +22,14 @@ def test_upload_and_download_with_encryption(tmpdir):
     from toil_scripts.lib.urls import s3am_upload
     from toil_scripts.lib.urls import download_url
     from boto.s3.connection import S3Connection, Bucket, Key
+    # Check if exists and delete if it does
+    # Try/Except not needed as delete() never raises exception
+    conn = S3Connection()
+    b = Bucket(conn, 'cgl-driver-projects')
+    k = Key(b)
+    k.key = 'test/upload_file'
+    if k.exists():
+        k.delete()
     work_dir = str(tmpdir)
     # Create temporary encryption key
     key_path = os.path.join(work_dir, 'foo.key')
@@ -35,14 +43,10 @@ def test_upload_and_download_with_encryption(tmpdir):
     s3_dir = 's3://cgl-driver-projects/test'
     s3am_upload(fpath=upload_fpath, s3_dir=s3_dir, s3_key_path=key_path)
     # Download the file
-    url = 'https://s3-us-west-2.amazonaws.com/cgl-driver-projects/test/upload_file'
+    url = 's3://cgl-driver-projects/test/upload_file'
     download_url(url=url, name='download_file', work_dir=work_dir, s3_key_path=key_path)
     download_fpath = os.path.join(work_dir, 'download_file')
     assert os.path.exists(download_fpath)
     assert filecmp.cmp(upload_fpath, download_fpath)
     # Delete the Key
-    conn = S3Connection()
-    b = Bucket(conn, 'cgl-driver-projects')
-    k = Key(b)
-    k.key = 'test/upload_file'
     k.delete()
