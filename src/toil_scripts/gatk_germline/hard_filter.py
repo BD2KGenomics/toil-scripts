30747060687c70e140b07a685043619166b697ca
Jacob Pfeil <jpfeil@ucsc.edu>
2016-08-25 17:28:48 -0700

    Run hard filter independently

--- src/toil_scripts/gatk_germline/hard_filter.py
+++ src/toil_scripts/gatk_germline/hard_filter.py
@@ -13,8 +13,7 @@ from toil_scripts.tools.variant_filters import gatk_select_variants, \
 
 def hard_filter_pipeline(job, uuid, gvcf_id, config):
     """
-    Runs GATK Hard Filtering on HaplotypeCaller output. Writes separate SNP and INDEL
-    VCF files to output directory.
+    Runs GATK Hard Filtering on a Genomic VCF file and uploads the results.
 
     0: GenotypeGVCFs            0 --> 1 --> 3 --> 5 --> 6
     1: Select SNPs                |           |
@@ -37,7 +36,7 @@ def hard_filter_pipeline(job, uuid, gvcf_id, config):
     genotype_gvcf_disk = PromisedRequirement(lambda gvcf: 2 * gvcf.size + human2bytes('5G'),
                                              gvcf_id)
     genotype_gvcf = job.wrapJobFn(gatk_genotype_gvcfs,
-                                  dict(uuid=gvcf_id),
+                                  {uuid: gvcf_id},
                                   config,
                                   cores=config.cores, memory=config.xmx, disk=genotype_gvcf_disk)
 
@@ -106,3 +105,73 @@ def hard_filter_pipeline(job, uuid, gvcf_id, config):
     merge_variants.addChild(input_vcf)
     merge_variants.addChild(output_vcf)
     return merge_variants.rv()
+
+
+def main():
+    """
+    Simple command line interface to run hard filtering on a single sample.
+    """
+    import argparse
+
+    from toil.job import Job
+
+    from toil_scripts.gatk_germline.germline import download_shared_files
+    from toil_scripts.lib.urls import download_url_job
+
+    parser = argparse.ArgumentParser(description=main.__doc__,
+                                     formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('--sample',
+                        default=None,
+                        nargs=2,
+                        type=str,
+                        help='Space delimited sample UUID and GVCF file in the format: uuid url')
+
+    parser.add_argument('--genome-fasta',
+                        required=True,
+                        type=str,
+                        help='Path or URL to genome fasta file')
+
+    parser.add_argument('--output-dir',
+                        default=None,
+                        help='Path/URL to output directory')
+
+    Job.Runner.addToilOptions(parser)
+    options = parser.parse_args()
+    options.cores = 8
+    options.xmx = '10G'
+    options.run_bwa = False
+    options.preprocess = False
+    options.run_vqsr = False
+    options.run_oncotator = False
+    options.synapse_login = None
+    options.ssec = None
+    options.suffix = ''
+    options.unsafe_mode = False
+    options.annotations = ['QualByDepth',
+                           'FisherStrand',
+                           'StrandOddsRatio',
+                           'ReadPosRankSumTest',
+                           'MappingQualityRankSumTest',
+                           'RMSMappingQuality',
+                           'InbreedingCoeff']
+
+    uuid, url = options.sample
+
+    shared_files = Job.wrapJobFn(download_shared_files, options).encapsulate()
+    download_sample = shared_files.addFollowOnJobFn(download_url_job,
+                                                    url,
+                                                    name='toil.g.vcf',
+                                                    synapse_login=None,
+                                                    s3_key_path=None,
+                                                    disk='25G')
+
+    download_sample.addFollowOnJobFn(hard_filter_pipeline,
+                                  uuid,
+                                  download_sample.rv(),
+                                  shared_files.rv())
+
+    Job.Runner.startToil(shared_files, options)
+
+if __name__ == '__main__':
+    main()
