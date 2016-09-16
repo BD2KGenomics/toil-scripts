46e5a06d88f827968403f4b03a4a6fcdaef0f344
CJ <cketchum@ucsc.edu>
2016-09-16 14:47:14 -0700

    Refer to proper version of toil-lib

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ kwargs = dict(
     author_email='cgl-toil@googlegroups.com',
     url="https://github.com/BD2KGenomics/toil-scripts",
     install_requires=[
-        'toil-lib==1.0.4a1.dev55',
+        'toil-lib==1.1.0a1.dev52',
         'pyyaml==3.11'],
     tests_require=[
         'pytest==2.8.3'],
